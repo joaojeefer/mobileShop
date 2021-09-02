600b468389be7aba6b1774f30a12302444a79470
João Jeefer <joao.felix@dbserver.com.br>
2021-09-01 23:39:26 -0300

    add global styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,29 @@
+const colors = {
+  primary: {
+    dark: '#1a212a',
+    default: '#293946',
+    light: '#338aa9',
+  },
+  secondary: {
+    dark: '#828a82',
+    default: '#ded4d0',
+    light: '#f8f9f7',
+  },
+  success: {
+    dark: '#33820d',
+    default: '#3fa110',
+    light: '#d7e6c8',
+  },
+  warning: {
+    default: '#ffcd00',
+    light: '#ffeb98',
+    dark: '#765f00',
+  },
+  danger: {
+    default: '#e60000',
+    light: '#ffb4b4',
+    dark: '#ab4745',
+  },
+};
+
+export default colors;
