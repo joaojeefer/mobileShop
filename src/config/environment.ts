c4e04eafef7de0153a551468796181ae504a95a9
João Jeefer <joao.felix@dbserver.com.br>
2021-09-06 10:47:44 -0300

    adjust baseURL

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,6 @@
 const env = {
   server: {
-    baseURL: 'https://5d6da1df777f670014036125.mockapi.io/api',
+    baseURL: 'http://5d6da1df777f670014036125.mockapi.io/api',
     fetchProducts: '/v1/product',
   },
 };
